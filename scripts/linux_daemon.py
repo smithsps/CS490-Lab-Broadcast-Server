f745dbeb6bb551633c97ce90d8297718a177ef07
Simon Smith <smithsps@gmail.com>
2016-02-19 02:30:11 -0500

    Simple http request and verify

---
+++ scripts/linux_daemon.py
@@ -0,0 +1,28 @@
+
+from hashlib import sha1
+import http.client
+import hmac
+import json
+
+#32-bit key for signing our messages
+auth_key = "7d28d176792b29f73855dbdef2d7d5b929315ab2b89e0225aaa52c61513e9edb"
+
+server_url = "localhost"
+server_port = 8000
+
+
+data = {"test" : "TEST"}
+
+
+conn = http.client.HTTPConnection(server_url, server_port)
+
+request_data = json.dumps(data)
+mac = hmac.new(str.encode(auth_key), str.encode(request_data), sha1)
+
+conn.request("PUT", "/linux", request_data, headers={"Auth": mac.hexdigest()})
+
+response = conn.getresponse()
+
+print(request_data)
+print(mac.hexdigest())
+print(response.status)
