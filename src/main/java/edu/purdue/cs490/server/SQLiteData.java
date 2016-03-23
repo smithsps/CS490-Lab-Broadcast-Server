6ea4311e21ae975bfb2457d3c9786ff6010c9ced
dolavesm <dolavesm@purdue.edu>
2016-03-23 17:22:35 -0400

    added functions for sql database for the server to use

--- src/main/java/edu/purdue/cs490/server/SQLiteData.java
+++ src/main/java/edu/purdue/cs490/server/SQLiteData.java
@@ -38,5 +38,194 @@ public class SQLiteData
     }
     return total;
   }
+  
+	public void updateLabPC(String labroom, String machine, int occupied)
+	{
+		Connection c = null;
+		Statement stmt = null;
+		String sql = null;
+		try {
+		  Class.forName("org.sqlite.JDBC");
+		  c = DriverManager.getConnection("jdbc:sqlite:test.db");
+		  System.out.println("Opened database successfully");
+		  c.setAutoCommit(false);
+
+		  stmt = c.createStatement();
+		  ResultSet rs = stmt.executeQuery(
+			"UPDATE "+labroom+" SET OCCUPIED = "+occupied+" WHERE MACHINE_NAME = '"+machine+"';");
+		  rs.close();
+		  stmt.close();
+		  c.close();
+		  
+		} catch ( Exception e ) {
+		  log.log(Level.FINE, e.getClass().getName() + ": " + e.getMessage() );
+		  System.exit(0);
+		}
+	}
+	public void addBroadcaster(String username, String room, String help)
+	{
+		Connection c = null;
+		Statement stmt = null;
+		String sql = null;
+		try {
+		  Class.forName("org.sqlite.JDBC");
+		  c = DriverManager.getConnection("jdbc:sqlite:test.db");
+		  System.out.println("Opened database successfully");
+		  c.setAutoCommit(false);
+
+		  stmt = c.createStatement();
+		  ResultSet rs = stmt.executeQuery("INSERT INTO BROADCASTER (USERNAME,ROOM,HELP)"+
+			"VALUES('"+username+"', '"+room+"', '"+help+"');");
+		  rs.close();
+		  stmt.close();
+		  c.close();
+		  
+		} catch ( Exception e ) {
+		  log.log(Level.FINE, e.getClass().getName() + ": " + e.getMessage() );
+		  System.exit(0);
+		}
+	}
+	
+	public void removeBroadcaster(String username)
+	{
+		Connection c = null;
+		Statement stmt = null;
+		String sql = null;
+		try {
+		  Class.forName("org.sqlite.JDBC");
+		  c = DriverManager.getConnection("jdbc:sqlite:test.db");
+		  System.out.println("Opened database successfully");
+		  c.setAutoCommit(false);
+
+		  stmt = c.createStatement();
+		  ResultSet rs = stmt.executeQuery("DELETE FROM BROADCASTERS WHERE USERNAME = '"+username+"';");
+		  rs.close();
+		  stmt.close();
+		  c.close();
+		  
+		} catch ( Exception e ) {
+		  log.log(Level.FINE, e.getClass().getName() + ": " + e.getMessage() );
+		  System.exit(0);
+		}
+	}
+	
+	public String grabAllBroadcasters()
+	{
+		Connection c = null;
+		Statement stmt = null;
+		String sql = null;
+		String allBroadcasters = null;
+		try {
+		  Class.forName("org.sqlite.JDBC");
+		  c = DriverManager.getConnection("jdbc:sqlite:test.db");
+		  System.out.println("Opened database successfully");
+		  c.setAutoCommit(false);
+
+		  stmt = c.createStatement();
+		  ResultSet rs = stmt.executeQuery("SELECT * FROM BROADCASTERS;");
+		  
+		  while ( rs.next() ) {
+			 allBroadcasters += rs.getString("USERNAME");
+			 allBroadcasters += " ";
+			 allBroadcasters += rs.getString("ROOM");
+			 allBroadcasters += " ";
+			 allBroadcasters += rs.getString("HELP");
+			 allBroadcasters += "\n";
+		  }
+		  rs.close();
+		  stmt.close();
+		  c.close();
+		  
+		} catch ( Exception e ) {
+		  log.log(Level.FINE, e.getClass().getName() + ": " + e.getMessage() );
+		  System.exit(0);
+		}
+		return allBroadcasters;
+	}
+	
+	public String grabSpecificBroadcasters(String course)
+	{
+		Connection c = null;
+		Statement stmt = null;
+		String sql = null;
+		String courseBroadcasters = null;
+		try {
+		  Class.forName("org.sqlite.JDBC");
+		  c = DriverManager.getConnection("jdbc:sqlite:test.db");
+		  System.out.println("Opened database successfully");
+		  c.setAutoCommit(false);
+
+		  stmt = c.createStatement();
+		  ResultSet rs = stmt.executeQuery("SELECT * FROM BROADCASTERS;");
+		  
+		  while ( rs.next() ) {
+			 if(rs.getString("HELP").contains(course)){
+				 courseBroadcasters += rs.getString("USERNAME");
+				 courseBroadcasters += " ";
+				 courseBroadcasters += rs.getString("ROOM");
+				 courseBroadcasters += " ";
+				 courseBroadcasters += rs.getString("HELP");
+				 courseBroadcasters += "\n";
+			 }
+		  }
+		  rs.close();
+		  stmt.close();
+		  c.close();
+		  
+		} catch ( Exception e ) {
+		  log.log(Level.FINE, e.getClass().getName() + ": " + e.getMessage() );
+		  System.exit(0);
+		}
+		return courseBroadcasters;
+	}
+	
+	public void addUser(String username, String courses, String current, String languages)
+	{
+		Connection c = null;
+		Statement stmt = null;
+		String sql = null;
+		try {
+		  Class.forName("org.sqlite.JDBC");
+		  c = DriverManager.getConnection("jdbc:sqlite:test.db");
+		  System.out.println("Opened database successfully");
+		  c.setAutoCommit(false);
+
+		  stmt = c.createStatement();
+		  ResultSet rs = stmt.executeQuery("INSERT INTO USERS (USERNAME,COURSES,CURRENT,LANGUAGES)"+
+			"VALUES('"+username+"', '"+courses+"', '"+current+"', '"+languages+"');");
+		  rs.close();
+		  stmt.close();
+		  c.close();
+		  
+		} catch ( Exception e ) {
+		  log.log(Level.FINE, e.getClass().getName() + ": " + e.getMessage() );
+		  System.exit(0);
+		}
+	}
+	
+	public void updateUserPreferences(String username, String courses, String current, String languages)
+	{
+		Connection c = null;
+		Statement stmt = null;
+		String sql = null;
+		try {
+		  Class.forName("org.sqlite.JDBC");
+		  c = DriverManager.getConnection("jdbc:sqlite:test.db");
+		  System.out.println("Opened database successfully");
+		  c.setAutoCommit(false);
+
+		  stmt = c.createStatement();
+		  ResultSet rs = stmt.executeQuery(
+			"UPDATE USERS SET COURSES = '"+courses+"', CURRENT = '"+current+"', LANGUAGES = '"+languages+
+			"' WHERE USERNAME = '"+username+"';");
+		  rs.close();
+		  stmt.close();
+		  c.close();
+		  
+		} catch ( Exception e ) {
+		  log.log(Level.FINE, e.getClass().getName() + ": " + e.getMessage() );
+		  System.exit(0);
+		}
+	}
 }
 
