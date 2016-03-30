3b6dd7caade56dccf56e2fccded3e41e8d81aeb2
dolavesm <dolavesm@purdue.edu>
2016-03-30 18:45:40 -0400

    cleaned up functions

--- src/main/java/edu/purdue/cs490/server/SQLiteJDBC.java
+++ src/main/java/edu/purdue/cs490/server/SQLiteJDBC.java
@@ -19,9 +19,9 @@ public class SQLiteJDBC
 
   public void createSQLdatabase()
   {
-    Connection c = null;
-    Statement stmt = null;
-    String sql = null;
+    Connection c;
+    Statement stmt;
+    String sql;
 
       //
     try {
@@ -209,62 +209,10 @@ public class SQLiteJDBC
 
       stmt.close();
       c.commit();
-      //c.close();
+      stmt.close();
 
-///////////////////////////////SELECT///////////////////////////////////////
-      stmt = c.createStatement();
-      ResultSet rs = stmt.executeQuery( "SELECT * FROM LABS;" );
-      while ( rs.next() ) {
-         String lab = rs.getString("LAB_ROOM");
-         String  mach = rs.getString("MACHINE_NAME");
-         int cnt  = rs.getInt("PC_AMOUNT");
-         String  os = rs.getString("OS");
-         log.finest("LAB_ROOM = " + lab);
-         log.finest("MACHINE_NAME = " + mach );
-         log.finest("PC_AMOUNT = " + cnt );
-         log.finest("OS = " + os );
-         log.finest("");
-      }
-      rs.close();
-      stmt.close();
-      
-      stmt = c.createStatement();
-      rs = stmt.executeQuery( "SELECT * FROM LWSNB146;" );
-      while ( rs.next() ) {
-         String mach = rs.getString("MACHINE_NAME");
-         int cnt  = rs.getInt("OCCUPIED");
-         log.finest("MACHINE_NAME = " + mach );
-         log.finest("");
-      }
-      rs.close();
-      stmt.close();
-      
       c.close();
 
-///////////////////////////////DELETE///////////////////////////////////////
-/*	  stmt = c.createStatement();
-      String sql = "DELETE from COMPANY where ID=2;";
-      stmt.executeUpdate(sql);
-      c.commit();
-
-      ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
-      while ( rs.next() ) {
-         int id = rs.getInt("id");
-         String  name = rs.getString("name");
-         int age  = rs.getInt("age");
-         String  address = rs.getString("address");
-         float salary = rs.getFloat("salary");
-         System.out.println( "ID = " + id );
-         System.out.println( "NAME = " + name );
-         System.out.println( "AGE = " + age );
-         System.out.println( "ADDRESS = " + address );
-         System.out.println( "SALARY = " + salary );
-         System.out.println();
-      }
-      rs.close();
-      stmt.close();
-      c.close();
-*/      
 ///////////////////////////////END///////////////////////////////////////      
     } catch ( SQLException e ) {
       log.severe("Error while creating SQL Table: "  + e.getMessage());
