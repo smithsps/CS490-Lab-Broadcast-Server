248efbcc103604e6580d159b05083180513a9749
Simon Smith <smithsps@gmail.com>
2016-04-08 00:38:23 -0400

    SSL for HTTPS for users api

--- src/main/java/edu/purdue/cs490/server/Server.java
+++ src/main/java/edu/purdue/cs490/server/Server.java
@@ -1,8 +1,15 @@
 package edu.purdue.cs490.server;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.ServerSocket;
+import java.security.KeyStore;
+import java.security.NoSuchAlgorithmException;
+import java.security.KeyStoreException;
+import java.security.KeyManagementException;
+import java.security.UnrecoverableKeyException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
@@ -12,14 +19,19 @@ import java.util.logging.Logger;
 
 import edu.purdue.cs490.server.api.ApiRunnable;
 import edu.purdue.cs490.server.api.Status;
-import edu.purdue.cs490.server.data.HTTPRequest;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
 
 
 public class Server {
     private static Server instance;
     ExecutorService executor;
     ServerSocket serverSocket;
+    ServerSocket serverSSLSocket;
     SQLiteData sqlData;
+
+    SSLContext sslContext;
 
     Map<String, ApiRunnable> api = new HashMap<>();
 
@@ -31,11 +43,15 @@ public class Server {
         }
         Server.instance = this;
 
+        sslInitialization();
+
         executor = Executors.newFixedThreadPool(60);
         try {
             serverSocket = new ServerSocket(5000);
+            serverSSLSocket = sslContext.getServerSocketFactory().createServerSocket(5001);
         } catch(IOException e){
             log.log(Level.SEVERE, "Unable to create server socket.", e);
+            System.exit(0);
         }
 
         SQLiteJDBC sqlcreate = new SQLiteJDBC();
@@ -51,6 +67,39 @@ public class Server {
         return Server.instance;
     }
 
+    private void sslInitialization(){
+        FileInputStream fileKeystore;
+        KeyStore keyStore;
+        KeyManagerFactory keyManagerFactory = null;
+
+        try {
+            sslContext = SSLContext.getInstance("TLS");
+        } catch (NoSuchAlgorithmException e) {
+            log.severe("TLS for SSL not available. Exiting..");
+            System.exit(0);
+        }
+
+        try {
+            fileKeystore = new FileInputStream("keystore.jks");
+            keyStore = KeyStore.getInstance("JKS");
+
+            keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
+            keyStore.load(fileKeystore, "cs490dev".toCharArray());
+            keyManagerFactory.init(keyStore, "cs490dev".toCharArray());
+
+            sslContext.init(keyManagerFactory.getKeyManagers(), null, null); //Null defaults
+        } catch (FileNotFoundException e) {
+            log.severe("Keystore file not found, possible misconfiguration.");
+            System.exit(0);
+        } catch (KeyStoreException | KeyManagementException | UnrecoverableKeyException e) {
+            log.severe("Unable to access key in keystore. Exiting..");
+            System.exit(0);
+        } catch (Exception e) {
+            log.severe("Unhandled exception during SSL Init.");
+            System.exit(0);
+        }
+    }
+
     public void apiPaths() {
         api.put("/", Status::handleStatus);
         api.put("/status", Status::handleStatus);
@@ -62,15 +111,31 @@ public class Server {
     }
 
     public void serverLoop() {
-        while(true) {
-            try {
-                Socket sock = this.serverSocket.accept();
-                HTTPHandler thread = new HTTPHandler(sock);
-                executor.execute(thread);
-            } catch(IOException e) {
-                log.log(Level.SEVERE, "Unable to create server socket.", e);
+        //HTTP Accept
+        executor.execute(() -> {
+            while(true) {
+                try {
+                    Socket sock = this.serverSocket.accept();
+                    HTTPHandler thread = new HTTPHandler(sock, false);
+                    executor.execute(thread);
+                } catch (IOException e) {
+                    log.log(Level.SEVERE, "Unable to create server socket.", e);
+                }
             }
-        }
+        });
+
+        //HTTPS Accept
+        executor.execute(() -> {
+            while(true) {
+                try {
+                    Socket sock = this.serverSSLSocket.accept();
+                    HTTPHandler thread = new HTTPHandler(sock, true);
+                    executor.execute(thread);
+                } catch (IOException e) {
+                    log.log(Level.SEVERE, "Unable to create server socket.", e);
+                }
+            }
+        });
     }
 
     public static void main(String[] args) {
